8f010769a38ff00f51bf8c384538f84663bb0ee6
Laraconverso <laraconverso@gmail.com>
2023-07-21 01:04:56 -0300

    update lesson 1

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Lara Converso on 21/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
