57c109828352f7a85b6d7df1dba863399d87fe67
Lara Converso <63512648+Laraconverso@users.noreply.github.com>
2024-07-19 02:02:43 -0300

    Basic Swift #1

---
+++ BasicsBootcamp.playground/Pages/Basics.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+var greeting = "Hello, playground"
+
+//simple console log
+print(greeting)
+
+/*
+let commentedCode = "this code will be commented in block"
+ */
+
+/*
+ in swift we use Camel Case, which means the
+ first letter is in lower case and the second firts letter
+ is in upper case
+*/
